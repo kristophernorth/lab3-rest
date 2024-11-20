f5be1502b8ed701fc8ec11eb188e04a7802deb05
kristophernorth <kristopher.north@gmail.com>
2024-11-20 15:10:46 -0700

    UML setup, I think

--- src/models/Mission.js
+++ src/models/Mission.js
@@ -1,7 +1,28 @@
 import { Schema } from "mongoose";
 
-export const MissionSchema = new Schema({
-  codename: { type: String, minLength: 1, maxLength: 100, required: true },
-  objective: { type: String, minLength: 1, maxLength: 100, required: true },
-  year: { type: String, minLength: 1, maxLength: 100, required: true },
-}, { toJSON: { virtuals: true }, timestamps: true })
+export const MissionSchema = new Schema(
+  {
+    codename: { type: String, minLength: 1, maxLength: 100, required: true },
+    objective: { type: String, minLength: 1, maxLength: 100, required: true },
+    year: { type: String, minLength: 1, maxLength: 100, required: true },
+    completed: { type: Boolean, required: true, default: false },
+    locationId: { type: Schema.ObjectId, required: true, ref: 'Mission' },
+    ratId: { type: Schema.ObjectId, required: true, ref: 'Mission' },
+  },
+
+  { toJSON: { virtuals: true }, timestamps: true }
+)
+
+MissionSchema.virtual('location', {
+  localField: 'locationId',
+  ref: 'Mission',
+  foreignField: '_id',
+  justOne: true
+})
+
+MissionSchema.virtual('rat', {
+  localField: 'ratId',
+  ref: 'Mission',
+  foreignField: '_id',
+  justOne: true
+})
--- src/models/Rat.js
+++ src/models/Rat.js
@@ -1,9 +1,9 @@
 import { Schema } from "mongoose";
 
-
 export const RatSchema = new Schema({
   name: { type: String, minLength: 1, maxLength: 100, required: true },
   callSign: { type: String, minLength: 1, maxLength: 100, required: true },
   picture: { type: String, minLength: 1, maxLength: 100, required: true },
   specialties: [{ type: String, maxLength: 25, required: true }]
+
 }, { toJSON: { virtuals: true }, timestamps: true })
